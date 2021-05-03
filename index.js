e5d8305d2e157cbf9fe71626ec518e308fd57138
DRGP <diego_gilbert@hotmail.com>
2021-05-03 16:02:06 -0500

    Lambda deployed from .zip, NLU service responsive

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
 exports.handler = async (event) => {
-    // TODO implement
-    let key = process.env.nluKey
-    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
-    const { IamAuthenticator } = require('ibm-watson/auth');
+    let nluKey = process.env.nluKey
+    let requestJSON = JSON.parse(event.body);
 
     const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
         version: '2020-08-01',
         authenticator: new IamAuthenticator({
-            apikey: key,
+            apikey: nluKey,
         }),
-        serviceUrl: 'https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/7188e9f2-890a-48a7-bd05-61e785967ba0',
+        serviceUrl: 'https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/16aea454-6f9a-4c9c-9233-1b5fcdbe4c07'
     });
 
     const analyzeParams = {
-        'text': event.historial_clinico,
+        'text': requestJSON.historial_clinico,
         'features': {
             'keywords': {
                 'sentiment': true,
                 'emotion': true,
-                'limit': 3
+                'limit': 5
+            },
+            'entities': {
+                'sentiment': true,
+                'limit': 5
             }
         }
     };
-
-    naturalLanguageUnderstanding.analyze(analyzeParams)
-        .then(analysisResults => {
-            console.log(JSON.stringify(analysisResults, null, 2));
-            return analysisResults;
-        })
-        .catch(err => {
-            console.log('error:', err);
-            return err;
-        });
+    
+    analysisResults = await naturalLanguageUnderstanding.analyze(analyzeParams);
+    return {
+        lenguaje_texto: analysisResults.result.language,
+        palabras_clave: analysisResults.result.keywords,
+        entidades: analysisResults.result.entities
+    };
 };
